2a96fa757ad8950375bef2589cbb1a3f05ef04d8
SamoraHunter <44898312+SamoraHunter@users.noreply.github.com>
2022-07-25 21:38:40 +0100

    Update KillDrivers.py

--- KillDrivers.py
+++ KillDrivers.py
@@ -1,5 +1,5 @@
 import wmi
-
+#import win32api
 
 def defineLocalServer():
     #
@@ -14,7 +14,7 @@ def defineLocalServer():
 def killChromeDrivers():
     try:
         for process in defineLocalServer().Win32_Process(name="chromedriver.exe"):
-            #process.Terminate()
+            process.Terminate()
             print('Process terminated')
             return print(process.Terminate())
     except:
@@ -35,5 +35,34 @@ def killFirefox():
             print("Failed on kill firefox")
         return print(process.Terminate())
 
+def killVlcDrivers():
+    try:
+        for process in defineLocalServer().Win32_Process(CommandLine="vlc.exe"):
+            #process.Terminate()
+            print(process)
+            #print('Process terminated')
+            #return print(process.Terminate())
+    except:
+        print("failed to kill")
 
-killChromeDrivers()
+def killPythonMPCProc():
+    try:
+        # for process in defineLocalServer().Win32_Process(CommandLine="python.exe"):
+        #     #process.Terminate()
+        #     print(process)
+            #print('Process terminated')
+            #return print(process.Terminate())
+        for process in defineLocalServer().Win32_Process(name="jupyter-notebook.exe"):
+            #print(process.ProcessId, process.Name)
+            if("MPC" in process.CommandLine):
+                print(process.CommandLine)
+                process.Terminate()
+
+    except Exception as e:
+        print("failed to kill", e)
+
+#killPythonMPCProc()
+
+#killVlcDrivers()
+#killChromeDrivers()
+#killGekoDrivers()
